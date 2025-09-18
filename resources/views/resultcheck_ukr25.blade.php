036f87894bc5f4e7b1c0fcb0aa569228694867cb
Friza Cahya <frizacp@gmail.com>
2025-09-18 23:34:57 +0700

    ukirun square

--- resources/views/resultcheck_ukr25.blade.php
+++ resources/views/resultcheck_ukr25.blade.php
@@ -47,7 +47,7 @@ function chipCode() {
 @section('header')
 <style>
 body {
-    background-image: url('/img/ukr25.png');
+    background-image: url('/img/ukr25_square.png');
     background-size: cover;
     background-repeat: no-repeat;
     background-position: top;
