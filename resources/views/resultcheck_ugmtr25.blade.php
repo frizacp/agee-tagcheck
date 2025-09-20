6f75140deb1c9e0c631c534c8ec44218ca7b5e0f
Friza Cahya <frizacp@gmail.com>
2025-09-20 19:35:53 +0700

    RESULT UGMTR25

--- resources/views/resultcheck_ugmtr25.blade.php
+++ resources/views/resultcheck_ugmtr25.blade.php
@@ -4,118 +4,131 @@
 
 @section('footer')
 <script>
-function chipCode() {
-    var code    =   $("#code").val()
+    function chipCode() {
+        var code = $("#code").val()
 
-    // setInterval #code disable for 5 second after scan tag to prevent duplicate
-    $("#code").attr("disabled", true);
-    setTimeout(function() {
-        $("#code").attr("disabled", false);
-        $("#code").focus();
-    }, 10000);
+        // setInterval #code disable for 5 second after scan tag to prevent duplicate
+        $("#code").attr("disabled", true);
+        setTimeout(function() {
+            $("#code").attr("disabled", false);
+            $("#code").focus();
+        }, 10000);
 
-    $.ajax({
-        url: "{{ route('tagcheck.index') }}",
-        type: "GET",
-        data: {
-            _token  :   "{{ csrf_token() }}",
-            code    :   code.substring(0, 24) ,
-            key     :   "show"
-        },
-        success: function(data) {
-            if (data.status == 200) {
-                $("#resultBib").html(data.data.bib);
-                $("#resultName").html(data.data.lastName);
-                $("#resultTime").html(data.data.time);
-                $("#contest").html(data.data.contest);
-                $("#pace").html(data.data.pace);
-            } else {
-                $("#resultBib").html("Not Found");
-                $("#resultName").html("Not Found");
-                $("#resultTime").html("Not Found");
-                $("#contest").html("Not Found");
-                $("#pace").html("Not Found");
+        $.ajax({
+            url: "{{ route('tagcheck.index') }}",
+            type: "GET",
+            data: {
+                _token: "{{ csrf_token() }}",
+                code: code.substring(0, 24),
+                key: "show"
+            },
+            success: function(data) {
+                if (data.status == 200) {
+                    $("#resultBib").html(data.data.bib);
+                    $("#resultName").html(data.data.lastName);
+                    $("#resultTime").html(data.data.time);
+                    $("#contest").html(data.data.contest);
+                    $("#pace").html(data.data.pace);
+                } else {
+                    $("#resultBib").html("Not Found");
+                    $("#resultName").html("Not Found");
+                    $("#resultTime").html("Not Found");
+                    $("#contest").html("Not Found");
+                    $("#pace").html("Not Found");
+                }
+
+                $("#code").val("")
             }
-
-            $("#code").val("")
-        }
-    });
-};
+        });
+    };
 </script>
 @endsection
 
 @section('header')
 <style>
-body {
-    background-image: url('/img/asnrun25.png');
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: top;
-    color: #131416!important;
-}
+    /* Menjadikan body sebagai flex container untuk centering */
+    html,
+    body {
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
 
-input {
-    background-color: transparent!important;
-    color: transparent!important;
-}
+    body {
+        /* Properti background dari kode Anda */
+        background-image: url('/img/ugmtr25_result.webp');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: top;
+        color: #131416;
 
-input:focus {
-    outline: none !important;
-    border:0;
-}
+        /* Properti untuk centering konten */
+        display: flex;
+        justify-content: center;
+        /* Center horizontal */
+        align-items: center;
+        /* Center vertikal */
+    }
 
-h2 {
-    font-size: 55px!important;
-    margin: 0!important;
-    font-weight: bold!important;
-}
+    /* Membuat input tidak terlihat namun tetap fungsional */
+    input#code {
+        background-color: transparent !important;
+        color: transparent !important;
+        border: 0;
+        outline: none !important;
+        cursor: default;
+        /* Menghilangkan cursor text */
+    }
 
-h3 {
-    font-size: 40px!important;
-    margin: 0!important;
-    font-weight: bold!important;
-}
+    /* Styling spesifik untuk konten di dalam .bibTag agar lebih rapi */
+    .bibTag h1,
+    .bibTag h2,
+    .bibTag h3,
+    .bibTag h4,
+    .bibTag h5 {
+        margin: 0;
+        font-weight: bold;
+    }
 
-h4 {
-    font-size: 75px!important;
-    margin: 0!important;
-    font-weight: bold!important;
-}
-h5 {
-    font-size: 40px!important;
-    margin: 0!important;
-    font-weight: bold!important;
-}
+    .bibTag h1 {
+        font-size: 100px;
+    }
 
-h1 {
-    font-size: 100px!important;
-    font-weight: bold!important;
-}
+    .bibTag h2 {
+        font-size: 55px;
+    }
 
-.bibTag {
-    padding-top: 20px
-}
+    .bibTag h3 {
+        font-size: 40px;
+    }
+
+    .bibTag h4 {
+        font-size: 75px;
+    }
+
+    .bibTag h5 {
+        font-size: 40px;
+    }
 </style>
 @endsection
 
 @section('content')
 <input type="text" class="border-0" autofocus style="width: 100%; height: 100%; position: fixed" autocomplete="off" id="code" onchange="chipCode()">
-<div class="bibTag">
-    <div class="text-center mt-4">
-        <img src="/img/logo_qrispr25.png" alt="Logo" style="max-width: 130px; margin-bottom: 10px;" class='mt-5'>
-        <h4 class="text-uppercase mt-3 mb-3" style="background-color: #990000; color: #FFFFFF ; padding-top: 10px; padding-bottom: 10px;">CONGRATULATION</h4>
-        <h2 class="text-uppercase mt-3 mb-3" id="resultName" style="color: #FFFFFF">resultName</h2>
+<div class="bibTag text-center">
+    <div class="mt-4">
+        <h4 class="text-uppercase mt-5 mb-3 px-5" style="background-color: #021C41; color: #FFFFFF ; padding-top: 10px; padding-bottom: 10px;">CONGRATULATION</h4>
+        <h2 class="text-uppercase mt-3" id="resultName" style="color: #1B1B1B">NAME</h2>
         <div class="d-flex justify-content-center align-items-center" style="gap: 60px; margin-top: 50px;">
             <div>
-            <h5 class="text-uppercase" id="contest" style="color: #FFFFFF">contest</h5>
-            <h2 class="text-uppercase" id="resultBib" style="color: #FFFFFF">bib</h2>
+                <h5 class="text-uppercase" id="contest" style="color: #1B1B1B">CONTEST</h5>
+                <h2 class="text-uppercase" id="resultBib" style="color: #1B1B1B">BIB</h2>
             </div>
-            <div style="border: 4px solid #FFFFFF; border-radius: 15px; padding: 20px;">
-                <h2 id="resultTime" style="color: #FFFFFF">resultTime</h2>
+            <div style="border: 4px solid #1B1B1B; border-radius: 15px; padding: 20px;">
+                <h2 id="resultTime" style="color: #1B1B1B">RESULT:TIME</h2>
             </div>
         </div>
     </div>
-    
+
 
     <div class="text-center">
     </div>
--- resources/views/tagcheck_ugmtr25.blade.php
+++ resources/views/tagcheck_ugmtr25.blade.php
@@ -4,104 +4,104 @@
 
 @section('footer')
 <script>
-function chipCode() {
-    var code    =   $("#code").val()
+    function chipCode() {
+        var code = $("#code").val()
 
-    // setInterval #code disable for 5 second after scan tag to prevent duplicate
-    $("#code").attr("disabled", true);
-    setTimeout(function() {
-        $("#code").attr("disabled", false);
-        $("#code").focus();
-    }, 10000);
+        // setInterval #code disable for 5 second after scan tag to prevent duplicate
+        $("#code").attr("disabled", true);
+        setTimeout(function() {
+            $("#code").attr("disabled", false);
+            $("#code").focus();
+        }, 10000);
 
-    $.ajax({
-        url: "{{ route('tagcheck.index') }}",
-        type: "GET",
-        data: {
-            _token  :   "{{ csrf_token() }}",
-            code    :   code.substring(0, 24) ,
-            key     :   "show"
-        },
-        success: function(data) {
-            if (data.status == 200) {
-                $("#resultBib").html(data.data.bib);
-                $("#resultName").html(data.data.lastName);
-                $("#resultTime").html(data.data.time);
-                $("#contest").html(data.data.contest);
-                $("#pace").html(data.data.pace);
-            } else {
-                $("#resultBib").html("Not Found");
-                $("#resultName").html("Not Found");
-                $("#resultTime").html("Not Found");
-                $("#contest").html("Not Found");
-                $("#pace").html("Not Found");
+        $.ajax({
+            url: "{{ route('tagcheck.index') }}",
+            type: "GET",
+            data: {
+                _token: "{{ csrf_token() }}",
+                code: code.substring(0, 24),
+                key: "show"
+            },
+            success: function(data) {
+                if (data.status == 200) {
+                    $("#resultBib").html(data.data.bib);
+                    $("#resultName").html(data.data.lastName);
+                    $("#resultTime").html(data.data.time);
+                    $("#contest").html(data.data.contest);
+                    $("#pace").html(data.data.pace);
+                } else {
+                    $("#resultBib").html("Not Found");
+                    $("#resultName").html("Not Found");
+                    $("#resultTime").html("Not Found");
+                    $("#contest").html("Not Found");
+                    $("#pace").html("Not Found");
+                }
+
+                $("#code").val("")
             }
-
-            $("#code").val("")
-        }
-    });
-};
+        });
+    };
 </script>
 @endsection
 
 @section('header')
 <style>
-body {
-    background-image: url('/img/ugmtr25_web.png');
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: top;
-    color: #131416!important;
-}
+    body {
+        background-image: url('/img/ugmtr25_web.png');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: top;
+        color: #131416 !important;
+    }
 
-input {
-    background-color: transparent!important;
-    color: transparent!important;
-}
+    input {
+        background-color: transparent !important;
+        color: transparent !important;
+    }
 
-input:focus {
-    outline: none !important;
-    border:0;
-}
+    input:focus {
+        outline: none !important;
+        border: 0;
+    }
 
-h2 {
-    font-size: 55px!important;
-    margin: 0!important;
-    font-weight: bold!important;
-}
+    h2 {
+        font-size: 55px !important;
+        margin: 0 !important;
+        font-weight: bold !important;
+    }
 
-h3 {
-    font-size: 40px!important;
-    margin: 0!important;
-    font-weight: bold!important;
-}
+    h3 {
+        font-size: 40px !important;
+        margin: 0 !important;
+        font-weight: bold !important;
+    }
 
-h4 {
-    font-size: 75px!important;
-    margin: 0!important;
-    font-weight: bold!important;
-}
-h5 {
-    font-size: 40px!important;
-    margin: 0!important;
-    font-weight: bold!important;
-}
+    h4 {
+        font-size: 75px !important;
+        margin: 0 !important;
+        font-weight: bold !important;
+    }
 
-h1 {
-    font-size: 100px!important;
-    font-weight: bold!important;
-}
+    h5 {
+        font-size: 40px !important;
+        margin: 0 !important;
+        font-weight: bold !important;
+    }
 
-.bibTag {
-    padding-top: 20px
-}
+    h1 {
+        font-size: 100px !important;
+        font-weight: bold !important;
+    }
+
+    .bibTag {
+        padding-top: 20px
+    }
 </style>
 @endsection
 
 @section('content')
 <div class="d-flex justify-content-center align-items-center" style="height: 100vh; flex-direction: column;">
     <input type="text" class="border-0" autofocus style="width: 100%; height: 100%; position: fixed" autocomplete="off" id="code" onchange="chipCode()">
-
     <div class="bibTag text-center">
         <div class='mt-5'>
             <h2 class="text-uppercase mt-3" id="resultName" style="color: #010101">Name</h2>
@@ -111,4 +111,3 @@ h1 {
     </div>
 </div>
 @endsection
-
